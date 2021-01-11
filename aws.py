fe2b3025f45f5672e82f4a17e46a074c6df9b9e8
Robert Kjaran <robert@kjaran.com>
2021-01-11 17:37:36 +0000

    Fix typo

--- aws.py
+++ aws.py
@@ -9,7 +9,7 @@ class Polly:
         self._session = Session(
             aws_access_key_id=os.environ["TIRO_TTS_AWS_ACCESS_KEY_ID"],
             aws_secret_access_key=os.environ["TIRO_TTS_AWS_SECRET_ACCESS_KEY"],
-            region=os.environ.get("TIRO_TTS_AWS_REGION", "eu-west-1"),
+            region_name=os.environ.get("TIRO_TTS_AWS_REGION", "eu-west-1"),
         )
         self._polly = self._session.client("polly")
 
