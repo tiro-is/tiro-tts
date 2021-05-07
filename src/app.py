344b07fd99aa555dd3f35309063f9720a655cc8e
Róbert Kjaran <robert@kjaran.com>
2021-05-07 17:20:14 +0000

    Fix gunicorn logging

--- src/app.py
+++ src/app.py
@@ -192,9 +192,10 @@ def route_index():
 
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
+
+if __name__ != "__main__":
     gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    if app.debug:
-        app.logger.setLevel(logging.DEBUG)
-    app.run()
