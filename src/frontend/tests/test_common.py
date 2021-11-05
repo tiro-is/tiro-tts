c033b687b06a26dd44e1f3ad2f2d8619299d2149
Smári Freyr Guðmundsson <smarifreyr30@gmail.com>
2021-11-05 16:10:47 +0000

    Wrote unit tests for src/frontend/common.py:consume_whitespace. Added tests for incorrect arguments.

--- src/frontend/tests/test_common.py
+++ src/frontend/tests/test_common.py
@@ -1,3 +1,5 @@
+from pytest import raises
+
 from ..common import utf8_byte_length, consume_whitespace, consume_whitespace_bytes
 
 class TestUtf8ByteLength:
@@ -30,9 +32,53 @@ class TestUtf8ByteLength:
 
     def test_ten(self):
         assert utf8_byte_length("1001") == 4
+
+    def test_eleven_incorrect_arg_int(self):
+        with raises(TypeError):
+            consume_whitespace(1337)
+
+    def test_twelve_incorrect_arg_dict(self):
+        with raises(TypeError):
+            consume_whitespace({'meaning': 42})
     
-def test_consume_whitespace():
-    assert True
+class TestConsumeWhitespace():
+    def test_one_empty_string(self):
+        assert consume_whitespace("") == (0,0)
+
+    def test_two_four_spaces(self):
+        assert consume_whitespace("    ") == (4,4)
+    
+    def test_three_space(self):
+        assert consume_whitespace(" ") == (1,1)
+
+    def test_four_tab_prefix(self):
+        assert consume_whitespace(" ragnar fór á fund") == (1,1)
+
+    def test_five_two_tab_prefix(self):
+        assert consume_whitespace("\t\tragnar fór á fund") == (2,2)
+
+    def test_six_four_spaces_prefix(self):
+        assert consume_whitespace("    afi minn fór á honum rauð") == (4,4)
+
+    def test_seven_tab_three_spaces_prefix(self):
+        assert consume_whitespace("\t   örlygur") == (4,4)
+
+    def test_eight_space_tab(self):
+        assert consume_whitespace(" \tragnar fór á fund   ") == (2,2)
+
+    def test_nine_space_postfix(self):
+        assert consume_whitespace("100 ") == (0,0)
+
+    def test_ten_tab_space_tab_postfix(self):
+        assert consume_whitespace("1001\t  \t") == (0,0)
+
+    def test_eleven_incorrect_arg_int(self):
+        with raises(TypeError):
+            consume_whitespace(1337)
+
+    def test_twelve_incorrect_arg_dict(self):
+        with raises(TypeError):
+            consume_whitespace({'meaning': 42})
 
 def test_consume_whitespace_bytes():
     assert True
