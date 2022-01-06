942b99e2b9a5000bbb3638872cc5dd302fd88d46
Smári Freyr Guðmundsson <smarifreyr30@gmail.com>
2022-01-06 17:06:26 +0000

    Changed the tests for SequiturGraphemeToPhonemeTranslator so the acutal raw output of the translate member function is tested against the defined expected output.

--- src/frontend/tests/test_grapheme_to_phoneme.py
+++ src/frontend/tests/test_grapheme_to_phoneme.py
@@ -7,9 +7,6 @@ LexiconGraphemeToPhonemeTranslator
 
 from ..lexicon import SimpleInMemoryLexicon
 
-def phoneseq_to_str(seq: "list[str]"):
-    return "".join(seq)
-
 class TestLexiconGraphemeToPhonemeTranslator:
     _language_code: str = "is-IS"
     _lex_path: Path = Path("external/test_models/lexicon.txt")
@@ -100,42 +97,67 @@ class TestSequiturGraphemeToPhonemeTranslator:
         }
     )
 
-    def translate_to_str(self, text: str) -> str:
-        return phoneseq_to_str(self._t.translate(text, self._language_code))
-
     def test_one_empty(self):
         text: str = ""
-        assert self.translate_to_str(text) == ""
+        assert self._t.translate(text, self._language_code) == []
 
     def test_two_multiword(self):
         text: str = "Halló heimur"
-        assert self.translate_to_str(text) == "halouheiːmʏr"
+        assert self._t.translate(text, self._language_code) == ['h', 'a', 'l', 'ou', 'h', 'eiː', 'm', 'ʏ', 'r']
 
     def test_three_incorrect_data_type(self):
         text: int = 15
         with raises(AttributeError):
-            self.translate_to_str(text)
+            self._t.translate(text, self._language_code)
 
     def test_four_word_lower_01(self):
         text = "kleprar"
-        assert self.translate_to_str(text) == "kʰlɛːprar"
+        assert self._t.translate(text, self._language_code) == ['kʰ', 'l', 'ɛː', 'p', 'r', 'a', 'r']
 
     def test_five_word_lower_02(self):
         text = "blöðrupumpur"
-        assert self.translate_to_str(text) == "plœðrʏpʰʏm̥pʏr"
+        assert self._t.translate(text, self._language_code) == ['p', 'l', 'œ', 'ð', 'r', 'ʏ', 'pʰ', 'ʏ', 'm̥', 'p', 'ʏ', 'r']
 
     def test_six_word_capital_first(self):
         text = "Blöðrupumpur"
-        assert self.translate_to_str(text) == "plœðrʏpʰʏm̥pʏr"
+        assert self._t.translate(text, self._language_code) == ['p', 'l', 'œ', 'ð', 'r', 'ʏ', 'pʰ', 'ʏ', 'm̥', 'p', 'ʏ', 'r']
 
     def test_seven_word_all_caps(self):
         text = "FJANDSAMLEGUR"
-        assert self.translate_to_str(text) == "fjantsamlɛɣʏr"
+        assert self._t.translate(text, self._language_code) == ['f', 'j', 'a', 'n', 't', 's', 'a', 'm', 'l', 'ɛ', 'ɣ', 'ʏ', 'r']
 
     def test_eight_foreign_char_01(self):
         text = "Султан"
-        assert self.translate_to_str(text) == ""
+        assert self._t.translate(text, self._language_code) == []
 
     def test_nine_foreign_char_02(self):
         text = "дlvдrlegt"
-        assert self.translate_to_str(text) == ""
+        assert self._t.translate(text, self._language_code) == []
+
+    def test_ten_sentence(self):
+        text = "ekki voru allir á sama máli um hvað ætti að gera við Vigfús"
+        assert self._t.translate(text, self._language_code)  \
+            == ['ɛ', 'h', 'c', 'ɪ', 'v', 'ɔː',
+                'r', 'ʏ', 'a', 't', 'l', 'ɪ',
+                'r', 'auː', 's', 'aː', 'm', 'a',
+                'm', 'auː', 'l', 'ɪ', 'ʏ', 'm',
+                'kʰ', 'v', 'aː', 'ð', 'ai', 'h',
+                't', 'ɪ', 'aː', 'ð', 'c', 'ɛː',
+                'r', 'a', 'v', 'ɪː', 'ð', 'v',
+                'ɪ', 'x', 'f', 'u', 's']
+
+    def test_eleven_sentence_punctuated(self):
+        text = "Flestum þótti Vigfús vera ómerkilegur, enda með eindæmum venjulegur í framkomu."
+        assert self._t.translate(text, self._language_code)  \
+            == ['f', 'l', 'ɛ', 's', 't', 'ʏ',
+                'm', 'θ', 'ou', 'h', 't', 'ɪ',
+                'v', 'ɪ', 'x', 'f', 'u', 's',
+                'v', 'ɛː', 'r', 'a', 'ouː', 'm',
+                'ɛ', 'r̥', 'c', 'ɪ', 'l', 'ɛ',
+                'ɣ', 'ʏ', 'r', 'sp', 'ɛ', 'n',
+                't', 'a', 'm', 'ɛː', 'ð', 'ei',
+                'n', 't', 'ai', 'm', 'ʏ', 'm',
+                'v', 'ɛ', 'n', 'j', 'ʏ', 'l',
+                'ɛ', 'ɣ', 'ʏ', 'r', 'iː', 'f',
+                'r', 'a', 'm', 'k', 'ɔ', 'm',
+                'ʏ', 'sp']
